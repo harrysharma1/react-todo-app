1b277d62089ae69aa426acbf89cdca887a65976e
Harry Sharma <harrysharma1066@gmail.com>
2023-02-23 00:39:22 +0000

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'https://harrysharma1.github.io/react-todo-app/',
   plugins: [react()],
 })
